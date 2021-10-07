927bb2db041f35ba35d2008c2df049cf47bd30de
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-06 21:48:38 -0300

    Criando objetos EntityManagerFactory e EntityManager

--- src/main/java/br/com/pedro/application/Main.java
+++ src/main/java/br/com/pedro/application/Main.java
@@ -2,6 +2,10 @@ package br.com.pedro.application;
 
 import br.com.pedro.model.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +18,9 @@ public class Main {
         System.out.println(pessoaDois);
         System.out.println(pessoaTres);
 
+        // aqui criamos o fabricador da conexão que estamos usando, por isso passar o nome da persistenceUnit que demos no persistrence.xml
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("con-cadastro-pesoas");
+        // aqui criamos o gerenciador dessa nossa conexão
+        EntityManager em = emf.createEntityManager();
     }
 }
