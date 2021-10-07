bbf5ff6d038854e76ff07d985abcc10735d05409
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-06 21:24:21 -0300

    Finalizando estrutura da inicial da classe Pessoa

--- src/main/java/br/com/pedro/model/Pessoa.java
+++ src/main/java/br/com/pedro/model/Pessoa.java
@@ -4,4 +4,43 @@ import java.io.Serializable;
 
 // usa essa interface para ter o ID da classe
 public class Pessoa implements Serializable {
+    // id da classe
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
