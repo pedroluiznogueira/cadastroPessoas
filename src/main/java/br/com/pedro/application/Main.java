6993a9748ee980da58cbc0ec3fb6a11f3d9daee1
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-06 21:26:58 -0300

    Criando objetos do tipo Pessoa com seus devidos atributos que futuramente serão transformados em conteúdos dos campos do banco de dados

--- src/main/java/br/com/pedro/application/Main.java
+++ src/main/java/br/com/pedro/application/Main.java
@@ -1,8 +1,14 @@
 package br.com.pedro.application;
 
+import br.com.pedro.model.Pessoa;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Pessoa pessoaUm = new Pessoa(1, "Kane", "pedro@pedro.com");
+        Pessoa pessoaDois = new Pessoa(1, "Luiz", "luiz@luiz.com");
+        Pessoa pessoaTres = new Pessoa(1, "Nogueira", "nogueira@nogueira.com");
 
 
     }
